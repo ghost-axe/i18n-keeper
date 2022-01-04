f88c834f678db2c85d27d77a456474491c12dd90
ghost-axe <ghost-axe@outlook.com>
2022-01-04 14:02:19 +0800

    修改

---
+++ index.js
@@ -0,0 +1,77 @@
+#! /usr/bin/env node
+const fs = require('fs')
+const express = require('express')
+const { program } = require('commander')
+const app = express()
+var port = 8678
+
+program.version('i18n-server 1.0.3 Crafted by 鬼斧')
+program
+  .option('-f, --file <type>', '多语言json文件路径')
+  .option('-p, --port <type>', '监听端口')
+program.parse(process.argv)
+
+const options = program.opts()
+if (!options.file) {
+  console.log("\033[41;30m ERROR \033[0;31m 命令参数错误 未指定多语言json文件名 运行i18n-server -h 查看用法\033[0m")
+  process.exit()
+}
+if (options.port) {
+  port = options.port
+}
+
+var lanJson = {}
+var timer = null
+let fileName = options.file
+let fileExist = fs.existsSync(fileName)
+
+if (fileExist) {
+  let text = fs.readFileSync('./zh-cn.json')
+  lanJson = JSON.parse(String(text))
+}
+
+var allowCors = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', '*')
+  res.header('Access-Control-Allow-Credentials','true')
+  next()
+}
+app.use(allowCors)
+
+app.get('/', async function(req, res) {
+  let keyPath = req.query.keyPath
+  let keys = keyPath.split('.')
+  let obj = lanJson
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i]
+    if (i == keys.length - 1) {
+      obj[key] = key
+    } else {
+      if (!obj[key]) {
+        obj[key] = {}
+      }
+      obj = obj[key]
+    }
+  }
+  if (timer) {
+    clearTimeout(timer)
+  }
+  timer = setTimeout(saveJson, 2000);
+  let resObj = {
+    code: 20000,
+    msg: 'success',
+    staus: 200
+  }
+  res.json(resObj)
+})
+
+app.listen(port, () => {
+  console.log(`i18n server is running at http://localhost:${port}`)
+})
+
+function saveJson() {
+  fs.writeFileSync(fileName, JSON.stringify(lanJson, null, '\t'), {
+    encoding: 'utf-8'
+  })
+}
---
+++ translate.js
@@ -0,0 +1,104 @@
+#! /usr/bin/env node
+const fs = require('fs')
+const { baidu } = require('translation.js')
+const { program } = require('commander')
+
+program.version('i18n-translate 1.0.3 Crafted by 鬼斧')
+program
+  .option('-file, --language-file <type>', '原语言json文件路径')
+  .option('-from, --from-language <type>', '原语言')
+  .option('-to, --to-language <type>', '翻译的语言，多个语言用英文逗号分隔')
+program.parse(process.argv)
+
+const options = program.opts()
+if (!options.languageFile) {
+  console.log("\033[41;30m ERROR \033[0;31m 命令参数错误 未指定原语言json文件路径 运行i18n-translate -h 查看用法\033[0m")
+  process.exit()
+}
+if (!options.fromLanguage) {
+  console.log("\033[41;30m ERROR \033[0;31m 命令参数错误 未指定原语言 运行i18n-translate -h 查看用法\033[0m")
+  process.exit()
+}
+if (!options.toLanguage) {
+  console.log("\033[41;30m ERROR \033[0;31m 命令参数错误 未指定翻译的语言 运行i18n-translate -h 查看用法\033[0m")
+  process.exit()
+}
+
+var workDir = ''
+var lanFile = options.languageFile
+var srcLan = options.fromLanguage
+var lanList = options.toLanguage.split(',')
+var translating = false
+var transData = []
+var transedData = []
+var SrcData = {}
+var stringData = ''
+var box = []
+var startTs = new Date().getTime()
+
+function translate () {
+  SrcData = JSON.parse(fs.readFileSync(lanFile))
+
+  console.log('翻译开始...')
+  start()
+
+  translating = true
+  stringData = transData.join('\n')
+  stringCut(stringData)
+
+  doTrans()
+}
+
+function start () {
+  for (let k in SrcData) {
+    travelObj(SrcData, k)
+  }
+}
+
+function travelObj (data, key) {
+  if (data[key] instanceof Object) {
+    for (let k in data[key]) {
+      travelObj(data[key], k)
+    }
+  } else {
+    transData.push(data[key])
+    if (translating) {
+      data[key] = transedData.shift()
+    }
+  }
+}
+
+function stringCut (data) {
+  if (data.length > 1800) {
+    let index = data.indexOf('\n', 1700)
+    box.push(data.substring(0, index))
+    stringCut(data.substring(index))
+  } else {
+    box.push(data)
+  }
+}
+
+async function doTrans () {
+  for (let lan of lanList) {
+    transedData = []
+    for (let data of box) {
+      let res = await baidu.translate({
+        text: data,
+        from: srcLan,
+        to: lan
+      })
+      for (let item of res.result) {
+        transedData.push(item)
+      }
+    }
+    start()
+    let disData = JSON.stringify(SrcData, null, '\t')
+    fs.writeFileSync(`./${lan}.json`, disData, 'utf8')
+    console.log(`./${lan}.json`)
+  }
+  let endTs = new Date().getTime()
+  let through = endTs - startTs
+  console.log("\n\033[42;30m DONE \033[0;32m " + "翻译完成，用时" + through + "ms\033[0m")
+}
+
+translate()
